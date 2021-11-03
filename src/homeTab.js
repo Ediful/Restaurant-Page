9d72c13b07e11401f64260ef84a4dac561d882a2
Ediful <56943939+Ediful@users.noreply.github.com>
2021-11-03 12:09:13 -0500

    added some images

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,44 +1,44 @@
 const homeTab = () => {
   let content = document.getElementById("content");
 
-  const navbar = document.createElement('nav');
+  const navbar = document.createElement("nav");
   content.appendChild(navbar);
 
-  const homeBtn = document.createElement('button');
-  homeBtn.textContent = 'Home';
-  homeBtn.id = 'homeBtn';
-  homeBtn.className = 'tab-link';
-  homeBtn.className = 'tab-active';
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  homeBtn.id = "homeBtn";
+  homeBtn.className = "tab-link";
+  homeBtn.className = "tab-active";
   navbar.appendChild(homeBtn);
 
-  const menuBtn = document.createElement('button');
-  menuBtn.id = 'menuBtn';
-  menuBtn.className = 'tab-link';
-  menuBtn.textContent = 'Menu';
+  const menuBtn = document.createElement("button");
+  menuBtn.id = "menuBtn";
+  menuBtn.className = "tab-link";
+  menuBtn.textContent = "Menu";
   navbar.appendChild(menuBtn);
 
-  const contactBtn = document.createElement('button');
-  contactBtn.id = 'contactBtn';
-  contactBtn.className = 'tab-link';
-  contactBtn.textContent = 'Contact';
+  const contactBtn = document.createElement("button");
+  contactBtn.id = "contactBtn";
+  contactBtn.className = "tab-link";
+  contactBtn.textContent = "Contact";
   navbar.appendChild(contactBtn);
 
-  const main = document.createElement('div');
+  const main = document.createElement("div");
   main.id = "main";
   content.appendChild(main);
 
-  const title = document.createElement('h1');
+  const title = document.createElement("h1");
   title.textContent = "Eddie's Icecream Shop";
   main.appendChild(title);
 
   const logo = document.createElement("IMG");
-  logo.src = "../images/ice-cream-selection.jpg";
+  logo.src = "images/home.svg";
   logo.alt = "Ice Cream Shop Logo";
   main.appendChild(logo);
 
-  const description = document.createElement('p');
+  const description = document.createElement("p");
   description.textContent = "Mmmh, this restaurant is delicious!";
   main.appendChild(description);
-}
+};
 
-export { homeTab }
+export { homeTab };
--- src/menuTab.js
+++ src/menuTab.js
@@ -1,54 +1,54 @@
 const menuTab = () => {
   let content = document.getElementById("content");
 
-  const navbar = document.createElement('nav');
+  const navbar = document.createElement("nav");
   content.appendChild(navbar);
 
-  const homeBtn = document.createElement('button');
-  homeBtn.textContent = 'Home';
-  homeBtn.id = 'homeBtn';
-  homeBtn.className = 'tab-link';
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  homeBtn.id = "homeBtn";
+  homeBtn.className = "tab-link";
   navbar.appendChild(homeBtn);
 
-  const menuBtn = document.createElement('button');
-  menuBtn.id = 'menuBtn';
-  menuBtn.className = 'tab-link';
-  menuBtn.className = 'tab-active';
-  menuBtn.textContent = 'Menu';
+  const menuBtn = document.createElement("button");
+  menuBtn.id = "menuBtn";
+  menuBtn.className = "tab-link";
+  menuBtn.className = "tab-active";
+  menuBtn.textContent = "Menu";
   navbar.appendChild(menuBtn);
 
-  const contactBtn = document.createElement('button');
-  contactBtn.id = 'contactBtn';
-  contactBtn.className = 'tab-link';
-  contactBtn.textContent = 'Contact';
+  const contactBtn = document.createElement("button");
+  contactBtn.id = "contactBtn";
+  contactBtn.className = "tab-link";
+  contactBtn.textContent = "Contact";
   navbar.appendChild(contactBtn);
 
-  const main = document.createElement('div');
-  main.id = 'main';
+  const main = document.createElement("div");
+  main.id = "main";
   content.appendChild(main);
 
-  const title = document.createElement('h1');
+  const title = document.createElement("h1");
   title.textContent = "Menu";
   main.appendChild(title);
 
+  const description = document.createElement("p");
+  description.textContent = "Here are the menu items!";
+  main.appendChild(description);
+
   const choc = document.createElement("IMG");
-  choc.src = "../images/ice-cream-chocolate.jpg";
+  choc.src = "images/Chocolate.svg";
   choc.alt = "Chocolate Icecream";
   main.appendChild(choc);
 
   const sundae = document.createElement("IMG");
-  sundae.src = "../images/ice-cream-sundae.jpg";
-  sundae.alt = "Chocolate Icecream";
+  sundae.src = "images/Vanilla.svg";
+  sundae.alt = "Vanilla Icecream";
   main.appendChild(sundae);
 
   const vannila_strawberry = document.createElement("IMG");
-  vannila_strawberry.src = "../images/ice-cream-vanilla-strawberry.jpg";
-  vannila_strawberry.alt = "Vanilla Strawberry Icecream";
+  vannila_strawberry.src = "images/strawberry.svg";
+  vannila_strawberry.alt = "Strawberry Icecream";
   main.appendChild(vannila_strawberry);
+};
 
-  const description = document.createElement('p');
-  description.textContent = "Here are the menu items!";
-  main.appendChild(description);
-}
-
-export { menuTab }
+export { menuTab };
